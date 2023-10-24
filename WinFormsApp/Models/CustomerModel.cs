ac923b1d9fbf3db073f1242be10c251000e7d849
THUAN\thuan <thuanpt182@gmail.com>
2023-10-24 22:35:50 +0700

    using thừa

--- WinFormsApp/Models/CustomerModel.cs
+++ WinFormsApp/Models/CustomerModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WinFormsApp.Models
+﻿namespace WinFormsApp.Models
 {
     public class CustomerModel
     {
