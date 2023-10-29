b3f931ddb94da483537643587ee20eeb358464c5
phatstran <hoangphat6302@gmail.com>
2023-10-29 22:34:59 +0700

    validator (customer, distributor, employee)

--- Services/Validators/CustomerValidator.cs
+++ Services/Validators/CustomerValidator.cs
@@ -7,7 +7,16 @@ namespace Services.Validators
     {
         public CustomerValidator() 
         {
-            
-        }
+			RuleFor(x => x.Phone)
+				.Must(phone => string.IsNullOrEmpty(phone) || phone.Length == 10)
+				.WithMessage("Số điện thoại phải có 10 ký tự hoặc để trống.");
+
+			RuleFor(x => x.Name)
+			   .NotEmpty().WithMessage("Tên là bắt buộc.")
+			   .MaximumLength(190).WithMessage("Tên không được vượt quá 190 ký tự.");
+
+			RuleFor(x => x.Address)
+				.MaximumLength(300).WithMessage("Địa chỉ không được vượt quá 300 ký tự.");
+		}
     }
 }
