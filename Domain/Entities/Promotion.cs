ac04e502e5013d05ec5b5322111daf2c46092745
THUAN\thuan <thuanpt182@gmail.com>
2023-11-10 21:41:00 +0700

    get status, type promotion

--- Domain/Entities/Promotion.cs
+++ Domain/Entities/Promotion.cs
@@ -43,5 +43,44 @@ namespace Domain.Entities
         [Column(TypeName = "nvarchar(5)")]
         [DefaultValue(TYPE_PERCENT)]
         public string? Status { get; set; }
+
+        public (string Key, string Value)[] GetStatusMapping(string? pStatus = null)
+        {
+            var statusMapping = new[]
+            {
+                (STATUS_DRAFT, "Nháp"),
+                (STATUS_APPROVED, "Đã duyệt"),
+                (STATUS_CANCEL, "Hủy bỏ")
+            };
+
+            if (pStatus != null)
+            {
+                var result = statusMapping.FirstOrDefault(item => item.Item1 == pStatus);
+
+                return result != default ? new[] { result } : statusMapping;
+            }
+
+            return statusMapping;
+        }
+
+        public (string Key, string Value)[] GetTypeMapping(string? pType = null)
+        {
+            var typeMapping = new[]
+            {
+                (TYPE_DISCOUNT, "Giảm giá"),
+                (TYPE_PERCENT, "Giảm phần trăm"),
+            };
+
+            if (pType != null)
+            {
+                var result = typeMapping.FirstOrDefault(item => item.Item1 == pType);
+
+                return result != default ? new[] { result } : typeMapping;
+            }
+
+            return typeMapping;
+        }
+
+
     }
 }
