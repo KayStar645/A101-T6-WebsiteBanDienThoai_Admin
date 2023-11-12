ca18df627c731bb9cda44ba2960a6920ab7eadc0
SonNV <sonnv1912@gmail.com>
2023-11-12 13:18:16 +0700

    chore: add column

--- WinFormsApp/Resources/Controls/Module/Order/OrderDetailControl.cs
+++ WinFormsApp/Resources/Controls/Module/Order/OrderDetailControl.cs
@@ -132,6 +132,8 @@ namespace WinFormsApp.Resources.Controls.Module.Order
                     item.ColorName,
                     item.CapacityName,
                     Util.AddCommas(item.Price, ""),
+                    Util.AddCommas(item.DiscountPrice, ""),
+                    Util.AddCommas(item.SumPrice, ""),
                     item.Quantity.ToString(),
                 };
 
@@ -154,12 +156,12 @@ namespace WinFormsApp.Resources.Controls.Module.Order
                 //await _orderService.ChangeTypeOrder(_order);
             }
 
-            Util.LoadControl(this, new ImportControl());
+            Util.LoadControl(this, new OrderControl());
         }
 
         private void Btn_Back_Click(object sender, EventArgs e)
         {
-            Util.LoadControl(this, new ImportControl());
+            Util.LoadControl(this, new OrderControl());
         }
 
         private void Text_Price_KeyUp(object sender, KeyEventArgs e)
