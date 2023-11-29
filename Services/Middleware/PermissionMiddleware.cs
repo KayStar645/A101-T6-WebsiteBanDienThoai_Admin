8ead4c808d8331408229a779296f3d199ab34d79
THUAN\thuan <thuanpt182@gmail.com>
2023-11-29 15:40:23 +0700

    new PermissionMiddleware

---
+++ Services/Middleware/PermissionMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Domain.ModelViews;
+using Services.Common;
+
+namespace Services.Middleware
+{
+    public class PermissionMiddleware
+    {
+        public PermissionMiddleware()
+        {
+        }
+
+        public bool CheckPermission()
+        {
+            AuthVM auth = ServiceCommon.AuthRespone;
+
+            return true;
+        }
+    }
+
+}
